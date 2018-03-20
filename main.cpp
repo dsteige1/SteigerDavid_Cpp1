19c1d6525be04194509817513598c73cc4feb853
dsteige1 <34238068+dsteige1@users.noreply.github.com>
2018-03-20 11:27:33 +0100

    Updated main
    Welcoming and direct link to menu().

--- main.cpp
+++ main.cpp
@@ -8,32 +8,11 @@ int menu();
 
 int main(){
 
+    cout << endl << "********************* Hello! *********************" << endl;
+    cout <<         "Welcome to Steiger's Car-Rental management system!" << endl;
+    cout <<         "**************************************************" << endl << endl;
+
     menu();
-
-    /*char action;
-
-    cout << endl << "**** Hello! ****" << endl << endl;
-    cout << "Please choose on of the following options" << endl;
-    cout << "1.     [p]rint the xml" << endl;
-    cout << "2.     p[r]int the txt" << endl;
-    cout << "3.     [c]onvert the the data sets into new file" << endl;
-    cout << "4.     [h]elp me!" << endl << endl;
-    cout << "Your choice: ";
-    cin >> action;
-
-    switch(action){
-        case 'p':
-            printXML();
-        break;
-        case 'r':
-            printTXT();
-        break;
-        case 'c':
-        break;
-        case 'h':
-            help();
-        break;
-    }*/
 
     return 0;
 }
