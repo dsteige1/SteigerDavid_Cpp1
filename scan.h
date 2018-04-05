a4ce2be856fad888b0ebaee0e73e670e608956df
dsteige1 <34238068+dsteige1@users.noreply.github.com>
2018-04-05 10:24:03 +0200

    Final Update (scan.h)
    Menu-function added.

--- scan.h
+++ scan.h
@@ -1,16 +1,17 @@
-//#include <iostream>
 #include <fstream>
 
 using namespace std;
 
 class ClscanData{
 private:
-    int anzahlScans;
     char *ID[10], *ort[20], *KM[10];
 public:
+    int anzahlScans;
     void druckeData();
     char *zeigeID(int id) {return ID[id];}
     char *zeigeOrt(int id) {return ort[id];}
     char *zeigeKM(int id) {return KM[id];}
-    void verarbeiteTXT(ifstream&);
+    void ladeTXT(ifstream&);
 };
+
+int menu();
