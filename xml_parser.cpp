3dd49f0c5249a4a799208153d4f0ba68a2e8d97f
dsteige1 <34238068+dsteige1@users.noreply.github.com>
2018-04-05 10:18:59 +0200

    Final Update (xml_parser.cpp)
    Getting rid of unused functions.

--- xml_parser.cpp
+++ xml_parser.cpp
@@ -5,20 +5,26 @@
 
 using namespace std;
 
+/*
+Parser and header (token.h) based on 'example6_sitzung8'.
+http://hki.uni-koeln.de/beispiel-6-rekursion-und-verknuepfte-listen-i/
+*/
+
 int printXML(){
     ifstream eingabeXML, eingabeDTD;
     ClToken *token;
 
     eingabeXML.open("databank_cars.xml");
-    eingabeDTD.open("databank1.dtd");
-
-    //verarbeiteDTD(eingabeDTD);
 
     token=new ClToken();
     if (token->getToken(eingabeXML)!=0) token->druckeToken(1);
 
-    eingabeDTD.close();
     eingabeXML.close();
+
+    cout << endl << "__________________________________________" << endl;
+    menu();
+
+    return 0;
 }
 
 int ClattToken::getAttList(char *eingabe) {
@@ -26,7 +32,7 @@ int ClattToken::getAttList(char *eingabe) {
     int zaehler;
     zustand zustand;
 
-    for (zaehler=0,zustand=inNamen, anzahlAtt=0; *eingabe!='\0'; eingabe = eingabe + 1) {
+    for (zaehler=0,zustand=inNamen, anzahlAtt=0; *eingabe!='\0'; eingabe = eingabe + 1){
         switch(*eingabe){
         case ' ':
             if (zustand == inNamen){
@@ -78,14 +84,14 @@ int ClattToken::getAttList(char *eingabe) {
 }
 
 
-int ClToken::getToken(ifstream &datei) {
+int ClToken::getToken(ifstream &datei){
     int zaehler;
     zustand zustand;
     char zeichen;
     char puffer[100];
     ClToken *child;
 
-    for (zaehler=0;;) {
+    for (zaehler=0;;){
         datei.get(zeichen);
         if(datei.eof()){
             if(*tokenName == '\0' && tokenChild == NULL && tokenInhalt == NULL)
@@ -103,23 +109,17 @@ int ClToken::getToken(ifstream &datei) {
                     strcpy(tokenInhalt,puffer);
                 }
             }
-            else
-            {
+            else{
                 datei.putback(zeichen);
-                if(*tokenName!='\0')
-                {
+                if(*tokenName!='\0'){
                     datei.putback('<');
-                    if(tokenChild==NULL)
-                    {
+                    if(tokenChild==NULL){
                         tokenChild=new ClToken;
                         tokenChild->getToken(datei);
                     }
-                    else
-                    {
-                        for(child=tokenChild;;child=child->tokenSibling)
-                        {
-                            if(child->tokenSibling==NULL)
-                            {
+                    else{
+                        for(child=tokenChild;;child=child->tokenSibling){
+                            if(child->tokenSibling==NULL){
                                 child->tokenSibling=new ClToken;
                                 child->tokenSibling->getToken(datei);
                                 break;
@@ -134,7 +134,7 @@ int ClToken::getToken(ifstream &datei) {
         case '>':
             puffer[zaehler]='\0';
             if(zustand==istEndTag) return 1;
-            if(zustand==istStartTag) {
+            if(zustand==istStartTag){
                 att.getAttList(puffer);
                 strcpy(tokenName,puffer);
             }
@@ -150,127 +150,23 @@ int ClToken::getToken(ifstream &datei) {
     }
 }
 
-void ClToken::druckeToken(int ebene)
-{
+void ClToken::druckeToken(int ebene){
     druckeTokenEbene(ebene);
-    cout << "Token: " << name() << " - " << inhalt() << endl;
-    if (att.zahlAtt() > 0) {
-        for (int i=0;i<att.zahlAtt();i++) {
+    cout << "Tag '" << name() << "' beinhaltet '" << inhalt() << "'." << endl;
+    if (att.zahlAtt() > 0){
+        for (int i=0;i<att.zahlAtt();i++){
             druckeTokenEbene(ebene);
-            cout << "Attribut " << att.zeigeAttName(i) << " hat den Wert " << att.zeigeAttWert(i) << endl;
+            cout << "Attribut '" << att.zeigeAttName(i) << "' hat den Wert '" << att.zeigeAttWert(i) <<"'." << endl;
         }
     }
     if(tokenChild!=NULL) tokenChild->druckeToken(ebene+1);
     if(tokenSibling!=NULL) tokenSibling->druckeToken(ebene);
+
 }
 
-void ClToken::druckeTokenEbene(int ebene)
-{
-    while (ebene > 0) {
+void ClToken::druckeTokenEbene(int ebene){
+    while (ebene > 0){
         cout << "| ";
         ebene = ebene - 1;
     }
 }
-
-struct element{
-    int zahl;
-    char name[100];
-    char tags[10][100];
-    char valid[100][100];
-};
-struct element *jetzt;
-
-void druckeElement(struct element *jetzt){
-    cout << endl << "Element: " << jetzt->name << " enthaelt die Tags: ";
-    for(int i=0;i<jetzt->zahl;i++){
-        if(i>0) cout << ", ";
-        cout << jetzt->tags[i];
-    }
-    cout << "." << endl;
-}
-
-void verarbeiteDTD(ifstream& datei){
-    char zeichen, letztes;
-    char puffer[100];
-    int zaehler;
-    enum zustandDTD {istStartTag, istEndTag, direktive, element, abhaengig, noise};
-    enum zustandDTD zustand = noise;
-    jetzt = new struct element;
-    jetzt->zahl = 0;
-
-    for(datei.get(zeichen); !datei.eof(); datei.get(zeichen)){
-        switch(zeichen){
-        case '<':
-            zustand = direktive;
-            zaehler=0;
-            break;
-        case '>':
-            if(zustand!=noise){
-                if(letztes!=' '){
-                    puffer[zaehler]='\0';
-                    strcpy(jetzt->tags[jetzt->zahl], puffer);
-                    strcpy(jetzt->valid[jetzt->zahl],puffer);
-                    jetzt->zahl++;
-                }
-                druckeElement(jetzt);
-            }
-            zustand=noise;
-            break;
-        case ' ':
-            if(letztes==' '){
-                continue;
-            }
-            puffer[zaehler]='\0';
-            zaehler=0;
-            switch(zustand){
-            case direktive:
-                if(strcmp(puffer, "!ELEMENT")){
-                    cout << "Fehler!" << endl;
-                    zustand = noise;
-                }
-                else {
-                    zustand = element;
-                }
-                break;
-            case element:
-                strcpy(jetzt->name, puffer);
-                strcpy(jetzt->valid[jetzt->zahl], puffer);
-                zustand = abhaengig;
-                break;
-            case abhaengig:
-                strcpy(jetzt->tags[jetzt->zahl], puffer);
-                strcpy(jetzt->valid[jetzt->zahl],puffer);
-                jetzt->zahl++;
-                break;
-            default:
-                break;
-            }
-            break;
-        default:
-            if(zustand!=noise){
-                puffer[zaehler] = zeichen;
-            }
-            zaehler++;
-            break;
-        }
-        letztes=zeichen;
-    }
-}
-
-/*int main(){
-    ifstream eingabeXML, eingabeDTD;
-    ClToken *token;
-
-    eingabeXML.open("databank_cars.xml");
-    eingabeDTD.open("databank1.dtd");
-
-    verarbeiteDTD(eingabeDTD);
-
-    token=new ClToken();
-    if (token->getToken(eingabeXML)!=0) token->druckeToken(1);
-
-    eingabeDTD.close();
-    eingabeXML.close();
-
-    return 0;
-}*/
