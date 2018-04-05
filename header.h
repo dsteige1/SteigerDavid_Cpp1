18c80829aa0f462dbbc734014f6406a4c974b4cb
dsteige1 <34238068+dsteige1@users.noreply.github.com>
2018-04-05 10:23:06 +0200

    Final Update (header.h)
    Exit-function added. 
    Cosmetic changes.

--- header.h
+++ header.h
@@ -3,7 +3,19 @@ using namespace std;
 
 int printXML();
 int printTXT();
-int help(){cout << endl << "Please type the bracketed character of the operation you want to launch. " << endl << endl;
-          return 0;
-          };
-//int menu();
+int help(){
+            int menu();
+            cout << endl << "??????????????????????????????????????????????????????????????????????????" << endl;
+            cout << endl << "Please choose an option an type the bracketed character of its description." << endl;
+            cout << endl << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl << endl;
+            menu();
+            return 0;
+            };
+int exit(){
+            cout << "***************************" << endl << endl;
+            cout << "Programm succesfully ended." << endl << endl;
+            cout << "***************************" << endl;
+            return 0;
+            };
+
+int convert();
